271553908b053f1a01b2944b7db7e26a7db7aff9
kalhara05 <kalhara139@gmail.com>
2025-12-11 11:40:28 +0530

    init

---
+++ app/src/main/java/com/example/roomer/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.roomer
+
+import android.os.Bundle
+import android.webkit.WebSettings
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        val settings: WebSettings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.databaseEnabled = true
+
+        webView.loadUrl("file:///android_asset/index.html")
+    }
+}
